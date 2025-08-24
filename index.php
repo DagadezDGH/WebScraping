2f2bb649496d57675f6dbd6d375b7608f2509064
Dagadez <davidgarridohdez@gmail.com>
2025-08-24 12:12:37 +0100

    Array with all the scrap
    We create an array with all the scraped content from the web and add DOMDocument and DOMXPATH for the use of queries

--- index.php
+++ index.php
@@ -9,7 +9,33 @@ curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($curl);
 
-echo $response;
+// echo $response;
 
 curl_close($curl);
+
+$response = mb_convert_encoding($response, 'HTML-ENTITIES', 'UTF-8');
+
+$dom = new DOMDocument();
+$dom->loadHTML($response);
+
+$xpath = new DOMXPath($dom);
+$ranks = $xpath->query('//span[contains(@class, "rank")]');
+$titles = $xpath->query('//span[contains(@class, "titleline")]/a');
+$scores = $xpath->query('//span[contains(@class, "score")]');
+
+$scrap_total = array();
+for ($i=0; $i < $ranks->length; $i++) {
+    $rank = trim($ranks->item($i)->nodeValue);
+    $title = trim($titles->item($i)->nodeValue);
+    $score = trim($scores->item($i)->nodeValue);
+
+    $scrap_total[] = array(
+        'rank' => $rank,
+        'title' => $title,
+        'npalabras' => str_word_count($title),
+        'score' => $score,
+    );
+}
+
+print_r($scrap_total);
 ?>
